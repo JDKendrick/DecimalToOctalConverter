00620d0b69e9c8600a74d2c38d718cdecc408b57
Jake Kendrick <43183524+JDKendrick@users.noreply.github.com>
2018-11-12 22:15:38 -0500

    Implemented the solution to this problem (see the repo description for details).

---
+++ DecimalToOctalConverter/DecimalToOctalConverter/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int octalConverter(int &);
+
+int main() {
+
+	int *number = new int;
+
+	cout << "Please input your decimal number: ";
+	cin >> *number;
+	cout << "Your number in Decimal is: " << *number << endl;
+
+	octalConverter(*number);
+
+	cout << "Your number in Octal is: " << *number << endl;
+
+	free(number);
+
+	return 0;
+}
+
+int octalConverter(int &x) {
+
+	string str = "";
+	int iterator = x;
+
+	while (iterator > 0) {
+		str = to_string(iterator % 8) + str;
+		iterator = floor(iterator / 8);
+	}
+
+	x = atoi(str.c_str());
+
+	return x;
+}
